371d6e32998a04592aed73d42268f6d798d2eee4
Reece Harris <reece.harris98@protonmail.com>
2021-08-13 13:02:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 const cthulhu = require('cthulhu.js')
 
 // Serve the Webpage '/' allowing only 'GET' requests
-cthulhu.server.serve('/', 'GET', function (req, res){
+cthulhu.server.serve('/', ['GET'], function (req, res){
     res.write('Hello World')   // Write hello to the page
 })
 
 // Start the server on port 8080, Default: 80
-cthulhu.server.start(8080)
+cthulhu.server.start(8080,true)
