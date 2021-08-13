0f645f08a1685d64c132f80198135a39d13718ab
Reece Harris <reece.harris98@protonmail.com>
2021-08-13 13:02:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Import the Cthulhu module
 const cthulhu = require('cthulhu.js')
 
-// Serve the Webpage '/' allowing only 'GET' requests
+// Serve the Webpage '/' allowing only 'GET' and 'POST' requests
 cthulhu.server.serve('/', ['GET'], function (req, res){
     res.write('Hello World')   // Write hello to the page
 })
 
-// Start the server on port 8080, Default: 80
+// Start the server on port 8080 in hardened mode, Default: 80,false
 cthulhu.server.start(8080,true)
